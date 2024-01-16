dbba92986631d417279c9dffcecc78b346a18863
nrraleeya <152852536+nrraleeya@users.noreply.github.com>
2024-01-17 02:44:28 +0800

    cleaningform.js

--- cleaningform.js
+++ cleaningform.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     var cleaningForm = document.getElementById('cleaningForm');
+    var maintenanceReport = document.getElementById('maintenanceReport');
 
     // Add an event listener for form submission
     cleaningForm.addEventListener('submit', function (event) {
@@ -8,5 +9,13 @@ document.addEventListener('DOMContentLoaded', function () {
         alert('Your toilet cleaning request has been submitted. Thank you!');
         
         cleaningForm.reset();
+    });
+
+    maintenanceReport.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        alert('Your maintenance report has been submitted. Thank you!');
+        
+        maintenanceReport.reset();
     });
 });
